93493af8eb643e38062589141d657a01d35b7425
EvanFricker <frickere@csu.fullerton.edu>
2019-08-24 14:34:32 -0700

    updated the text from pay per hour to profit from candy bars

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -4,18 +4,18 @@
 
 int main()
 {
-  double hours, rate, pay;
+  double bars, profit, pay;
 
   // Get the number of hours worked.
-  std::cout << "How many hours did you work? ";
-  std::cin >> hours;
+  std::cout << "How many candy bars did you sell? ";
+  std::cin >> bars;
 
   // Get the hourly pay rate.
-  std::cout << "How much do you get paid per hour? ";
-  std::cin >> rate;
+  std::cout << "How much does your organization earn for each candy bar sold? ";
+  std::cin >> profit;
 
   // Calculate the pay.
-  pay = hours * rate;
+  pay = bars * profit;
 
   // Display the pay.
   std::cout << "You have earned $" << pay << std::endl;
